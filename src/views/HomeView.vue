a6eaf1271c23d11b2ae28dda7c20886bcb164c9c
liyuelian <liyuelian.ke@outlook.com>
2023-03-10 21:43:01 +0800

    09-添加家居表单前端校验

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -42,20 +42,20 @@
             名字为name，必须需要和后端Javabean属性一致
       4.在前端中，对象的属性是可以动态生成的-->
     <el-dialog title="提示" v-model="dialogVisible" width="40%">
-      <el-form :model="form" label-width="120px">
-        <el-form-item label="家居名">
+      <el-form :model="form" label-width="120px" :rules="rules" ref="form">
+        <el-form-item label="家居名" prop="name">
           <el-input v-model="form.name" style="width: 80%"></el-input>
         </el-form-item>
-        <el-form-item label="厂商">
+        <el-form-item label="厂商" prop="maker">
           <el-input v-model="form.maker" style="width: 80%"></el-input>
         </el-form-item>
-        <el-form-item label="价格">
+        <el-form-item label="价格" prop="price">
           <el-input v-model="form.price" style="width: 80%"></el-input>
         </el-form-item>
-        <el-form-item label="销量">
+        <el-form-item label="销量" prop="sales">
           <el-input v-model="form.sales" style="width: 80%"></el-input>
         </el-form-item>
-        <el-form-item label="库存">
+        <el-form-item label="库存" prop="stock">
           <el-input v-model="form.stock" style="width: 80%"></el-input>
         </el-form-item>
       </el-form>
@@ -100,7 +100,31 @@ export default {
       search: '',//检索条件，可以在进行分页时保留上次的检索条件
       dialogVisible: false,
       form: {},//定义一个空表单
-      tableData: []
+      tableData: [],
+      //定义添加表单的校验规则
+      rules: {
+        name: [//这里我们可以添加多个针对name属性的规则校验
+          {required: true, message: "请输入家居名", tigger: "blur"}
+        ],
+        maker: [
+          {required: true, message: "请输入厂商", tigger: "blur"}
+        ],
+        price: [
+          {required: true, message: "请输入价格", tigger: "blur"},
+          //使用正则表达式对输入的数字进行处理
+          {pattern: /^([1-9]\d*|0)(\.\d+)?$/, message: "请输入数字", tigger: "blur"}
+        ],
+        sales: [
+          {required: true, message: "请输入销量", tigger: "blur"},
+          //使用正则表达式对输入的数字进行处理
+          {pattern: /^([1-9]\d*|0)$/, message: "请输入数字", tigger: "blur"}
+        ],
+        stock: [
+          {required: true, message: "请输入库存", tigger: "blur"},
+          //使用正则表达式对输入的数字进行处理
+          {pattern: /^([1-9]\d*|0)$/, message: "请输入数字", tigger: "blur"}
+        ]
+      }
     }
   },
   created() {//生命周期函数
@@ -112,6 +136,8 @@ export default {
       this.dialogVisible = true;
       //每次点击都要清空上一次的表单数据
       this.form = {};
+      //清空上次校验的信息
+      this.$refs['form'].resetFields();
     },
     save() {//将填写的表单数据发送给后端
       //修改和添加走的同一个方法
@@ -137,12 +163,30 @@ export default {
           this.list();
         })
       } else {//添加业务
-        //第一个参数为url，第二个参数是请求携带的数据
-        request.post("/api/save", this.form).then(res => {
-          console.log("res-", res)
-          this.dialogVisible = false;//隐藏表单
-          //调用list方法，刷新页面显示的数据
-          this.list();
+        //表单验证是否通过
+        this.$refs['form'].validate((valid) => {
+          //valid就是表单校验结果
+          if (valid) {//如果校验通过
+            //第一个参数为url，第二个参数是请求携带的数据
+            request.post("/api/save", this.form).then(res => {
+              console.log("res-", res)
+              this.dialogVisible = false;//隐藏表单
+              //调用list方法，刷新页面显示的数据
+              this.list();
+              //提示
+              this.$message({
+                type: "success",
+                message: "添加成功"
+              })
+            })
+          } else {//校验没有通过
+            //提示
+            this.$message({
+              type: "error",
+              message: "验证失败，不提交"
+            })
+            return false;//放弃提交
+          }
         })
       }
     },
